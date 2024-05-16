b5a846349fcce340688f08cd43d4d811e2612172
Shubham Rout <116967052+Shubh04tech@users.noreply.github.com>
2024-05-16 16:58:17 +0530

    Add files via upload

---
+++ ass5.garbageCollection/q2.java
@@ -0,0 +1,10 @@
+public class q2 {
+    public static void main(String[] args) {
+        String s = "abcd";
+        s.replace("a", "s");
+        System.out.println(s);
+        CharSequence cs = s;
+        System.out.println(cs.subSequence(0, 3));
+        System.out.println(cs.length());
+    }
+}
